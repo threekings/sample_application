14d07e08665b0dc2366fc857564343818cc377f6
threekings <3thkings@gmail.com>
2011-06-10 14:12:48 +0800

    copy blueprint

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Help")
+    end
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
